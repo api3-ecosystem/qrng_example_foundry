bdfdefb33472c6035e3503c3e8da2f37968d41ab
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-12-05 18:49:13 -0800

    update deployer

--- script/DeployQRNG.s.sol
+++ script/DeployQRNG.s.sol
@@ -10,12 +10,13 @@ contract DeployQRNG is Script {
     function run() external returns (Qrng) {
         vm.startBroadcast();
         //deploy with a variable
-       // Qrng qrng = new Qrng();
+                                    //Network Airnode Address
+        Qrng qrng = new Qrng(address(0x1));
         vm.stopBroadcast();
-        // return (qrng);
+        return (qrng);
     }
 }
 
 
 // Deployment line direct
-//forge create --rpc-url $env:MUMBAI_RPC_URL --private-key $env:PRIVATE_KEY --etherscan-api-key $env:POLYGON_ETHERSCAN_API_KEY --verify  contracts/PriceFeed.sol:PriceFeed 
+//forge create --rpc-url $env:PROVIDER_URL --private-key $env:PRIVATE_KEY --etherscan-api-key $env:ETHERSCAN_API_KEY --verify  src/QRNG.sol:Qrng 
