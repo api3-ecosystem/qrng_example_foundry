db0719ec0101013e9e0b69abaaa06f65f10b2a61
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-12-06 17:57:15 -0800

    update notes

--- test/QRNG.t.sol
+++ test/QRNG.t.sol
@@ -23,6 +23,8 @@ contract QRNGTest is Test {
     function setUp() public {
         fork1 = vm.createFork("https://rpc.ankr.com/eth_goerli");
         vm.selectFork(fork1);
+        // We must mimic an Airnode to call back to our QRNG contract
+        // So we deploy an airnodeRrp contract
         airnodeRrp = new AirnodeRrpV0();
         vm.startPrank(msg.sender);
         qrng = new Qrng(address(airnodeRrp));
@@ -47,7 +49,7 @@ contract QRNGTest is Test {
         qrng.setRequestParameters(airnode, endpointIdUint256, endpointIdUint256Array, sponsorWallet);
         vm.stopPrank();
 
-        uint256 blockNumberToReset = block.number;
+        //uint256 blockNumberToReset = block.number;
         vm.startPrank(address(qrng), address(qrng));
         // staticCall - simulate call to but not make the call to keep the request count
         bytes32 requestId = qrng.makeRequestUint256();
@@ -62,7 +64,12 @@ contract QRNGTest is Test {
 
     function testfulfillUint256() public {
 
-        // creating a local airnode to just the 
+        /* creating a local airnode for this particular test
+           Because of onlyAirnodeRrp modifier on the fulfillUint256 function 
+           Their airnode must be the one that calls and signs the function
+
+           airnode is called airnodelocal to not conflict with the global airnode name
+        */
         (address airnodelocal, uint256 airnodePrivateKey) = makeAddrAndKey("airnode");
     
 
@@ -74,12 +81,21 @@ contract QRNGTest is Test {
         bytes32 requestId = qrng.makeRequestUint256();
         vm.stopPrank();
 
+        //sponsor wallet will call the fulfill function
         vm.startPrank(sponsorWallet);
+        //We are just returning a random number for this test
         uint256 randomNumber = 3244353535;
+        //Building the response
         bytes memory data = abi.encode(randomNumber);
         bytes32 messageHash = keccak256(abi.encodePacked(requestId, data)).toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(airnodePrivateKey, messageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
+        //Calling the fulfill function via the sponsor wallet without response
         airnodeRrp.fulfill(requestId, airnodelocal, address(qrng), qrng.fulfillUint256.selector, data, signature);
+        vm.stopPrank();
+        vm.startPrank(msg.sender);
+        uint256 returnedNumber = qrng.getRandomNumber();
+        vm.stopPrank();
+        console.log("returnedNumber: ", returnedNumber);
     }
 }
